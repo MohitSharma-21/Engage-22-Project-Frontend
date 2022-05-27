060dfa4fce19adabbbfc64a7a9d1b636b988cb30
MOHIT KUMAR SHARMA <mohitkumar.sharma.ece20@itbhu.ac.in>
2022-05-27 13:51:10 +0530

    added deployed server-api

--- utils/axios.jsx
+++ utils/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/";
+// const API_BASE_URL = "http://localhost:5000/";
+const API_BASE_URL = "https://picture-and-todo-manager.herokuapp.com/";
 
 var axiosInstance = axios.create({
   baseURL: API_BASE_URL,
