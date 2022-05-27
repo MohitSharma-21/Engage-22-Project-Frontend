6885094426f5a00331743ac0fc7cf0c651b64adc
MOHIT KUMAR SHARMA <mohitkumar.sharma.ece20@itbhu.ac.in>
2022-05-27 22:56:57 +0530

    sign-in-up-home-css-bug-2

--- components/sign-in/modal.jsx
+++ components/sign-in/modal.jsx
@@ -101,10 +101,11 @@ const SignInModal = ({ username, setUsername, modalIsOpen, setModalsOpen }) => {
                 boxColor: "green",
                 lineWidth: 3,
               });
-
+              
+              toast.dismiss();
               setToken(token);
               clearInterval(intervalID);
-              router.replace("/");
+              router.replace("/gallery");
 
             } else {
 
