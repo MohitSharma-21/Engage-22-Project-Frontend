77351c0324437507e410ded7d65db88df488bef1
Mohit Sharma <82596483+MohitSharma-21@users.noreply.github.com>
2023-05-09 20:48:24 +0530

    Updated render backend api axios.jsx

--- utils/axios.jsx
+++ utils/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5000/";
-const API_BASE_URL = "https://picture-and-todo-manager.herokuapp.com/";
+// const API_BASE_URL = "https://picture-and-todo-manager.herokuapp.com/";
+const API_BASE_URL = "https://engage-backend.onrender.com/";
 
 var axiosInstance = axios.create({
   baseURL: API_BASE_URL,
